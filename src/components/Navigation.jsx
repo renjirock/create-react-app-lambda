5fdabf071ecab2256bb68338d9b0eb8da071e293
renjirock <mauriciokode@gmail.com>
2022-05-04 18:39:41 -0500

    f

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -11,6 +11,7 @@ class Navigation extends React.Component {
         event.preventDefault();
     }
     render() {
+        const login = localStorage.getItem("login");
         return (
             <Fragment>
                 <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
@@ -19,7 +20,10 @@ class Navigation extends React.Component {
                         <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                         <div className="collapse navbar-collapse" id="navbarResponsive">
                             <ul className="navbar-nav ms-auto my-2 my-lg-0">
-                                <li className="nav-item"><button  className="btn btn-primary" onClick={this.handleSubmit}>Salir</button ></li>
+                                {login === 'true' ? 
+                                    <li className="nav-item"><button  className="btn btn-primary" onClick={this.handleSubmit}>Salir</button ></li>
+                                 : ''}
+                                
                             </ul>
                         </div>
                     </div>
