72deb989eeda48d549041049084c5a8932a137dc
renjirock <mauriciokode@gmail.com>
2022-05-04 20:21:06 -0500

    FORM

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,34 @@
 import React, { Fragment } from "react";
 
 class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputState: "",
+            inputAddress: "",
+            inputLevel: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value,
+        });
+    }
+    handleSubmit(event) {
+        localStorage.setItem("inputState", this.state.inputState);
+        localStorage.setItem("inputAddress", this.state.inputAddress);
+        localStorage.setItem("inputLevel", this.state.inputLevel);
+        console.log(this.state.inputState);
+        console.log(this.state.inputAddress);
+        console.log(this.state.inputLevel);
+        event.preventDefault();
+    }
   render() {
     document.body.style.backgroundColor = "#212529"; 
     const studentId = localStorage.getItem("studentId")
@@ -9,18 +37,18 @@ class Form extends React.Component {
             <section class="page-section bg-dark text-white">
                 <div class="container px-4 px-lg-5 text-center">
                     <h2 class="mb-4">Contesta sa la siguente encuesta</h2>
-                    <form class="row g-3">
+                    <form class="row g-3" onSubmit={this.handleSubmit}>
                         <div class="col-md-6">
-                            <label for="inputEmail4" class="form-label">Id del studiante</label>
-                            <input type="text" readonly class="form-control-plaintext text-white text-center" id="staticEmail" value={studentId}/>
+                            <label for="staticEmail" class="form-label">Id del studiante</label>
+                            <input type="text" readonly class="form-control-plaintext text-white text-center" name="staticEmail" id="staticEmail" value={studentId}/>
                         </div>
                         <div class="col-md-6">
                             <label for="inputState" class="form-label">¿En qué sede asiste la mayoría del tiempo?</label>
-                            <select id="inputState" class="form-select">
-                            <option selected>selecciona una...</option>
-                            <option>csu</option>
-                            <option>jardin</option>
-                            <option>salud</option>
+                            <select id="inputState" name="inputState" class="form-select">
+                                <option selected>selecciona una...</option>
+                                <option>csu</option>
+                                <option>jardin</option>
+                                <option>salud</option>
                             </select>
                         </div>
                         <div class="col-12">
@@ -28,8 +56,8 @@ class Form extends React.Component {
                             <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
                         </div>
                         <div class="col-12">
-                            <label for="inputAddress2" class="form-label"> ¿A qué nivel pasa en inglés?</label>
-                            <select id="inputState" class="form-select">
+                            <label for="inputLevel" class="form-label"> ¿A qué nivel pasa en inglés?</label>
+                            <select id="inputLevel" name="inputLevel" class="form-select">
                                 <option selected>selecciona una...</option>
                                 <option>A1</option>
                                 <option>A21</option>
--- src/components/admin/Topbar.jsx
+++ src/components/admin/Topbar.jsx
@@ -61,15 +61,6 @@ class Topbar extends React.Component {
                         </button>
                         <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                             aria-labelledby="userDropdown">
-                            <button className="dropdown-item" >
-                                <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Perfil
-                            </button>
-                            <button className="dropdown-item" >
-                                <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Configuracion
-                            </button>
-                            <div className="dropdown-divider"></div>
                             <button  className="dropdown-item" onClick={this.handleSubmit}>
                                 <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                 Logout
